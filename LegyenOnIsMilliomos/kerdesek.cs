13570125b15c212387b662d5bcd585574b6097d5
Vidilosz <molnar.bence@petrik.hu>
2025-05-16 16:09:43 +0200

    kerdesek fele

---
+++ LegyenOnIsMilliomos/kerdesek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegyenOnIsMilliomos
+{
+    public class Kerdesek
+    {
+        public List<Kerdes> MindenKerdes { get; set; }
+
+        public Kerdesek()
+        {
+            MindenKerdes = new List<Kerdes>();
+        }
+
+        public void BetoltKerdesekFajlbol(string filepath)
+        {
+        }
+    }
+}
