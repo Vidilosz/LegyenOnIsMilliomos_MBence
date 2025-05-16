1e571652e38bb550514245e134f11066a82206d2
Vidilosz <molnar.bence@petrik.hu>
2025-05-16 16:00:02 +0200

    sorkerdes class

---
+++ LegyenOnIsMilliomos/sorkerdes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegyenOnIsMilliomos
+{
+    public class Sorkerdes
+    {
+        public string Kerdes { get; set; }
+        public List<string> Valaszok { get; set; }
+        public List<int> HelyesSorrend { get; set; } // például: [2, 3, 1, 4]
+
+        public Sorkerdes(string kerdes, List<string> valaszok, List<int> helyesSorrend)
+        {
+            Kerdes = kerdes;
+            Valaszok = valaszok;
+            HelyesSorrend = helyesSorrend;
+        }
+
+        public bool EllenorizValaszt(List<int> jatekosValasza)
+        {
+            return HelyesSorrend.SequenceEqual(jatekosValasza);
+        }
+    }
+}
